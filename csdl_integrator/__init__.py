72d0ff3ddee05cb1582eb84fd670dbd6ff11a85b
Sankalp Kaushik <sskaushi@ucsd.edu>
2024-11-21 19:43:21 -0800

    Updated init.py

--- csdl_integrator/__init__.py
+++ csdl_integrator/__init__.py
@@ -1,3 +1,3 @@
-from .integrators import trapezoidIntegrator, rk4Integrator, csdlIntegrator
+from .integrators import trapezoidIntegrator, rk4Integrator, integrate
 
-__all__ = ['trapezoidIntegrator', 'rk4Integrator', 'csdlIntegrator']
+__all__ = ['trapezoidIntegrator', 'rk4Integrator', 'integrate']
